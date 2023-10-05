e20358651ce48dbe603bb1cdf9c76c5fc22e6048
dewibela <dewibelisa@gmail.com>
2023-10-05 17:17:00 +0700

    first init github

---
+++ application/views/homepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            margin: 25px;
+            padding: 25px;
+            width: 600px;
+            background: #d22276;
+            color: black;
+            border-radius: 30px;
+        }
+        .eee{
+            font-size: 20px;
+            text-align: center;
+        }
+        </style>
+</head>
+<body>
+    <div>
+        <?php
+        echo '<p class="eee">Perkenalan</p>';
+        echo "<p>Nama : Dewi Belisa</p>";
+        echo "<p>Nim : 12220039</p>";
+        echo "<p>Kelas : 12.3A.13</p>";
+        echo "<p>Alamat : Bubulak</p>";
+        echo "</p>Hobi : Berenang</p>";?>
+</body>
+</html>
